30460b1a1e457b6792cdc0f8cf12ca45d22d314f
neo14250 <49986089+neo14250@users.noreply.github.com>
2020-06-27 09:45:06 +0800

    Update playlist.js

--- YT/playlist.js
+++ YT/playlist.js
@@ -1,12 +1,12 @@
 //YouTube影片ID
 let playList = [
-    "4iVW7OhZ0p8", //讓我留在你身邊
-    "7NN4RlKspW0", //全放空
-    "KvwY9DN7LxY" //飄向北方
+    "nNrOCtcoIMU",
+    "KvwY9DN7LxY",
+    "KvwY9DN7LxY" 
 ];
 //播放起訖秒數
 let playTime = [
     [57,65],
     [69,79],
-    [76,80]  
+    [26,40]  
 ];
